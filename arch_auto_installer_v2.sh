5c23d555380c166622ac29da4744ddcfb3342703
Jason Pittman <jasonpit@mac.com>
2025-05-19 13:15:38 -0700

    added some stuff

--- arch_auto_installer_v2.sh
+++ arch_auto_installer_v2.sh
@@ -49,8 +49,13 @@ partprobe "$DISK"
 sleep 2
 
 # Assign partition variables after partitioning
-EFI_PART="${DISK}1"
-ROOT_PART="${DISK}2"
+if [[ "$DISK" == *"nvme"* ]]; then
+  EFI_PART="${DISK}p1"
+  ROOT_PART="${DISK}p2"
+else
+  EFI_PART="${DISK}1"
+  ROOT_PART="${DISK}2"
+fi
 
 # === Format and mount ===
 mkfs.vfat -F32 "$EFI_PART"
